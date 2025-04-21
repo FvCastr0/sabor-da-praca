098ab45c666875f45705440456c04f48012084e5
FvCastr0 <fvdc2008@gmail.com>
2025-04-21 16:12:38 -0300

    update cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,12 +7,7 @@ const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors({
-    origin: ["https://sabor-da-praca.vercel.app", "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-  });
+  app.enableCors();
 
   await app.init();
 
