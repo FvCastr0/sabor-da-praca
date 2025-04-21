c9e13cea48d04677721a93493b1324a619c9a4b5
FvCastr0 <fvdc2008@gmail.com>
2025-04-21 16:09:33 -0300

    update cors origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ const server = express();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
-    origin: ["https://sabor-da-praca.vercel.app/", "http://localhost:3000"],
+    origin: ["https://sabor-da-praca.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
